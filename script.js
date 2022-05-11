97f9c546c0d04488a3861f9ba043ceb31f7c153a
NighIsTheEnd <jonathan.c.evey@gmail.com>
2022-05-10 17:12:31 -0700

    add basic styling all four gid btns work

--- script.js
+++ script.js
@@ -1,29 +1,69 @@
 const gridBox = document.getElementById('grid-container');
-let boxCount = 16;
-function creatGrid() {  
-    for (let i = 0; i < boxCount; i++) {
+
+
+const gridSizeBtns = document.querySelectorAll('.grid-size-btn');
+/*---remove original grid and makes a new one that is selected---*/
+gridSizeBtns.forEach(addBtnEvent);
+
+/*Colors box on grid when the mouse moves over it*/
+function colorBoxes(){
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => {
+        box.addEventListener('mouseover', () => {
+            box.classList.add('colored');
+        })
+    })
+}
+
+/*-------Removes color when a box is clicked--------*/
+function removeColor() {
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => {
+        box.addEventListener('click', () => {
+            box.classList.remove('colored');
+        })
+    })
+}
+
+/*---------Generates the starting drawing area grid----------*/
+function creatGrid(num) {  
+    for (let i = 0; i < num; i++) {
         const div = document.createElement('div');
-        div.className = 'box';
+        div.className = `box grid-${num}`;
         gridBox.append(div);
-        for (let j = 1; j < boxCount; j++) {
+        for (let j = 1; j < num; j++) {
             const div = document.createElement('div');
-            div.className = 'box';
+            div.className = `box grid-${num}`;
             gridBox.append(div);
         }
     }
+}    
+
+
+function addBtnEvent(btn) {
+    btn.addEventListener('click', () => {
+        for (let i = gridBox.children.length - 1; i >= 0; i--) {
+            if (gridBox.hasChildNodes()) {
+                gridBox.removeChild(gridBox.children[0]);
+              }
+        }
+        if(btn.id === "btn-16") {
+            creatGrid(16);
+        }
+        if(btn.id === "btn-24") {
+            creatGrid(24);
+        }
+        if(btn.id === "btn-32") {
+            creatGrid(32);
+        }
+        if(btn.id === "btn-48") {
+            creatGrid(48);
+        } 
+        colorBoxes();
+        removeColor();
+    }) 
 }
-creatGrid();
 
-const boxes = document.querySelectorAll('.box');
-
-boxes.forEach(box => {
-    box.addEventListener('mouseover', () => {
-        box.classList.add('colored');
-    })
-})
-
-boxes.forEach(box => {
-    box.addEventListener('click', () => {
-        box.classList.remove('colored');
-    })
-})
+window.onload = creatGrid(16);
+colorBoxes();
+removeColor();
